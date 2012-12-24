11d66f9afcfd2fe01676ba0d7308dd66bb95129b
Jodi Dias <Jodi@Jodis-MacBook-Pro.local>
2012-12-24 15:45:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JodiTest::Application.config.secret_token = '5b8339e8b2ccffabdf05df07fb61f00f29abcf177eea4cf1ccb94a4fb501b76552ef7ee6ea3c110c2870f4a7796bb8e964a936a988f8d3207a5352af90661f7e'
